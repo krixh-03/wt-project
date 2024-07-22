1c85846481c82c640682446d13db9f620747b293
krishna <krishsai0303@gmail.com>
2024-07-22 15:58:05 +0530

    added tanstack router dependencies and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Login from "./components/Login";
 
 export default function App() {
-  return <>
+  return <div id="root">
     <Login /> 
-  </>
+  </div>
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,25 +1,32 @@
 export default function Login() {
-  
   return (
     <div className="flex min-h-screen flex-col justify-center items-center">
-      <div className="border-2 rounded p-10 border-black bg-zinc-200">
+      <div className="flex flex-col border-2 rounded p-10 border-black bg-zinc-200">
+        <div className="flex justify-start">
+          <div className="text-2xl font-semibold pb-1 underline underline-offset-2">Login</div>
+        </div>
         <div className="flex flex-col">
-        <div>
-          <label className="font-semibold">Username</label> <br />
-          <input
-            type="text"
-            className="mb-2 rounded border-2 border-black"
-          />
-          <br />
-          <label className="font-semibold">Password</label> <br />
-          <input
-            type="text"
-            className="mb-3 rounded border-2 border-black"
-          />
-        </div>
-        <div className="flex flex-col items-center">
-        <button className="border-black border-2 rounded w-20 bg-purple-400 ">submit</button>
-        </div>
+          <div>
+            <label className="font-semibold">Username</label> <br />
+            <input
+              type="text"
+              className="mb-2 rounded border-2 border-black"
+              placeholder="kaushik"
+            />
+            <br />
+            <label className="font-semibold">Password</label> <br />
+            <input
+              type="password"
+              className="mb-3 rounded border-2 border-black"
+              placeholder="********"
+             
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <button className="border-black border-2 rounded w-20 bg-purple-400 ">
+              submit
+            </button>
+          </div>
         </div>
       </div>
     </div>
